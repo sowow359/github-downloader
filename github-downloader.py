75fcadd7aefd6643e369b7c6db5f1c56afd7ce45
sowow359 <sowow359@ya.ru>
2023-01-31 03:06:51 +0300

    make it simpler i guess

--- github-downloader.py
+++ github-downloader.py
@@ -11,9 +11,36 @@ from collections import OrderedDict
 from pathlib import Path
 from urllib import request
 
-from helpers import run_once_per, reporthook
-
 GITHUB_API_BASE_URL = "https://api.github.com/repos"
+
+
+def run_once_per(seconds):
+    """
+    Allows function to run again only after specified number of seconds.
+    """
+
+    last_run: float = float('-inf')
+
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            nonlocal last_run
+
+            passed = time.time() - last_run
+            time.sleep(max(seconds - passed, 0))
+
+            result = function(*args, **kwargs)
+            last_run = time.time()
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def reporthook(block_num, block_size, total_size):
+    downloaded = block_num * block_size
+    if downloaded < total_size:
+        print(int(downloaded / total_size * 100), '%', end="\r", sep='')
 
 
 def get_args():
