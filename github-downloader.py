152fa31df15308e4340554fac744002ec8406880
sowow359 <sowow359@ya.ru>
2023-02-10 03:51:47 +0300

    dataclasses, per-file release processing

--- github-downloader.py
+++ github-downloader.py
@@ -10,7 +10,9 @@ import socket
 import sys
 import time
 from collections import OrderedDict
+from dataclasses import dataclass
 from pathlib import Path
+from typing import List, Optional
 from urllib.error import HTTPError, ContentTooShortError
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen, urlretrieve
@@ -18,6 +20,66 @@ from urllib.request import Request, urlopen, urlretrieve
 socket.setdefaulttimeout(10)
 
 GITHUB_API_BASE_URL = "https://api.github.com/repos"
+
+PRERELEASE = "prerelease"
+STABLE = "stable"
+
+
+@dataclass
+class AssetInfo:
+    size: int
+    name: str
+    url: Optional[str]
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        url = d["browser_download_url"]
+        asset_name = d["name"]
+        size = d["size"]
+
+        return cls(
+            name=asset_name,
+            url=url,
+            size=size
+        )
+
+
+@dataclass
+class ReleaseInfo:
+    tag: str
+    assets: List[AssetInfo]
+    readme: str
+    release_type: str
+    zipball_url: str
+    tarball_url: str
+    created_at: str
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        assets = [AssetInfo.from_dict(info) for info in d["assets"]]
+
+        readme = inspect.cleandoc(
+            f"""
+                # {d['name']}
+                        
+                Github Release link: {d['html_url']}
+                
+                created_at = {d['created_at']}
+                
+                published_at = {d['published_at']}
+                
+                # Release notes
+        """
+        ) + d["body"]
+        return cls(
+            tag=d["tag_name"].replace('/', '_'),
+            assets=assets,
+            readme=readme,
+            release_type=PRERELEASE if d["prerelease"] else STABLE,
+            zipball_url=d["zipball_url"],
+            tarball_url=d["tarball_url"],
+            created_at=d['created_at'],
+        )
 
 
 class BaseError(Exception):
@@ -61,14 +123,14 @@ def reporthook(count, block_size, total_size):
         start_time = time.time()
         return
     duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / (1024 * 1024 * duration))
+    downloaded = int(count * block_size)
+    speed = int(downloaded / (1024 * 1024 * duration))
 
-    percent = int(count * block_size * 100 / total_size) if total_size else '...'
+    percent = int(count * block_size * 100 / total_size) if total_size >= 0 else '...'
 
     print(
         f"\r{percent}%, "
-        f"{progress_size / (1024 * 1024):.2f} MB, "
+        f"{downloaded / (1024 * 1024):.2f} MB, "
         f"{speed:.1f} MB/s, "
         f"{duration:.1f} seconds passed",
         end=''
@@ -126,21 +188,20 @@ def get_as_json(url):
         return json.loads(content)
 
 
-def get_latest(repo: str):
+def get_latest(repo: str) -> Optional[ReleaseInfo]:
     try:
         latest = get_as_json(f"{GITHUB_API_BASE_URL}/{repo}/releases/latest")
     except HTTPError as e:
         if e.code == 404:
             # no releases
-            return {}
+            return None
         else:
             raise e
 
-    latest["tag_name"] = latest["tag_name"].replace('/', '_')
-    return latest
+    return ReleaseInfo.from_dict(latest)
 
 
-def get_last_n_releases(repo: str, n: int = 50):
+def get_last_n_releases(repo: str, n: int = 50) -> List[ReleaseInfo]:
     params = {"per_page": n}
     encoded_params = urlencode(params)
 
@@ -148,10 +209,11 @@ def get_last_n_releases(repo: str, n: int = 50):
 
     for release in releases:
         release["tag_name"] = release["tag_name"].replace('/', '_')
-    return releases
+
+    return [ReleaseInfo.from_dict(d) for d in releases]
 
 
-def get_releases(repo: str, release_type: str) -> dict:
+def get_releases(repo: str, release_type: str) -> List[ReleaseInfo]:
     # https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#list-releases
 
     all_releases = OrderedDict()
@@ -159,29 +221,29 @@ def get_releases(repo: str, release_type: str) -> dict:
     latest_release = get_latest(repo)
     if not latest_release:
         # no latest release -> no releases
-        return {}
+        return []
     # always keep one tagged with `latest` first
-    all_releases[latest_release["tag_name"]] = latest_release
+    all_releases[latest_release.tag] = latest_release
 
-    last_n = get_last_n_releases(repo)
-    for release in last_n:
-        all_releases[release["tag_name"]] = release
+    last_n: list[ReleaseInfo] = get_last_n_releases(repo)
 
-    return (
-        all_releases
+    all_of_them: list[ReleaseInfo] = [latest_release] + [r for r in last_n if r.tag != latest_release.tag]
+    result: list[ReleaseInfo] = (
+        all_of_them
         if release_type == "all"
-        else OrderedDict((k, v) for k, v in all_releases.items() if not v["prerelease"])
+        else list(filter(lambda r: r.release_type == STABLE, all_of_them))
     )
 
+    return result
 
-def get_local_versions(home: str, repo: str):
+
+def get_local_versions(home: str, repo: str) -> List[str]:
     print(f"Listing existing local versions")
     repo_dir = os.path.join(home, repo)
     if not os.path.exists(repo_dir):
         return []
 
-    versions = os.listdir(repo_dir)
-    print(f"Got {len(versions)} local versions: {versions}")
+    versions = [item for item in os.listdir(repo_dir) if not item.startswith('.')]
     return list(sorted(versions, reverse=True))
 
 
@@ -208,8 +270,8 @@ def download_file(url: str, to: str):
     raise DownloadError(f"Error downloading {url}")
 
 
-def download(release_info: dict, home: str, repo: str):
-    release_path = os.path.join(home, repo, release_info["tag_name"])
+def download(release_info: ReleaseInfo, home: str, repo: str):
+    release_path = os.path.join(home, repo, release_info.tag)
     try:
         _download_release(release_info=release_info, release_path=release_path)
         return
@@ -220,55 +282,70 @@ def download(release_info: dict, home: str, repo: str):
     except Exception as ex:
         print(f"Unexpected error: {ex.__class__.__name__}: {ex}")
 
-    print(f"Cleaning up: removing {release_path}")
-    shutil.rmtree(release_path)
     exit(1)
 
 
-def _download_release(release_info: dict, release_path: str):
+def _download_release(release_info: ReleaseInfo, release_path: str):
     if not os.path.exists(release_path):
         Path(release_path).mkdir(parents=True)
         print(f"Created release dir {release_path}")
 
-    for asset in release_info["assets"]:
-        url = asset["browser_download_url"]
-        asset_name = asset["name"]
+    existing_assets_with_sizes = {
+        n: os.path.getsize(os.path.join(release_path, n))
+        for n in os.listdir(release_path)
+        if n not in ("README.md", "source.zip", "source.tar.gz")
+    }
 
-        filepath = os.path.join(release_path, asset_name)
-        print(f"Downloading {asset['name']} to {filepath}")
-        download_file(url, filepath)
+    release_assets_with_sizes = {
+        i.name: i.size
+        for i in release_info.assets
+    }
 
-    tarball_path = os.path.join(release_path, "source.tar.gz")
-    print(f"Downloading source tarball to {tarball_path}")
-    download_file(release_info["tarball_url"], tarball_path)
-
-    zipball_path = os.path.join(release_path, "source.zip")
-    print(f"Downloading source zipball to {zipball_path}")
-    download_file(release_info["zipball_url"], zipball_path)
-
-    print(f"Creating release README.md file")
-    with open(os.path.join(release_path, "README.md"), "w") as f:
-        content = inspect.cleandoc(
-            f"""
-                # {release_info['name']}
-                        
-                Github Release link: {release_info['html_url']}
-                
-                created_at = {release_info['created_at']}
-                
-                published_at = {release_info['published_at']}
-                
-                # Release notes
-        """
+    to_download_names = [
+        name
+        for name, size in release_assets_with_sizes.items()
+        if (
+                name not in existing_assets_with_sizes
+                or size != existing_assets_with_sizes[name]
         )
-        f.write(content)
-        f.write('\n\n')
-        f.write(release_info["body"])
+    ]
+
+    to_download_assets = [
+        i
+        for i in release_info.assets
+        if i.name in to_download_names
+    ]
+
+    for asset in to_download_assets:
+        filepath = os.path.join(release_path, asset.name)
+        print(f"Downloading {asset.name} to {filepath}")
+        download_file(asset.url, filepath)
+
+    tarball_filename = "source.tar.gz"
+    tarball_path = os.path.join(release_path, tarball_filename)
+    if not os.path.exists(tarball_path):
+        print(f"Downloading source tarball to {tarball_path}")
+        download_file(release_info.tarball_url, tarball_path)
+
+    zipball_filename = "source.zip"
+    zipball_path = os.path.join(release_path, zipball_filename)
+    if not os.path.exists(zipball_path):
+        print(f"Downloading source zipball to {zipball_path}")
+        download_file(release_info.zipball_url, zipball_path)
+
+    readme_filename = "README.md"
+    readme_path = os.path.join(release_path, readme_filename)
+    if not os.path.exists(readme_path):
+        print(f"Creating release README.md file")
+        with open(readme_path, "w") as f:
+            f.write(release_info.readme)
+
+    print("All good")
 
 
 def run(home: str, repo: str, n_releases: int, release_type: str):
     try:
-        github_releases = get_releases(repo=repo, release_type=release_type)
+        github_releases: list[ReleaseInfo] = get_releases(repo=repo, release_type=release_type)[:n_releases]
     except HTTPError as he:
         print(f"Error getting releases for {repo}: {he}")
         print("Exiting")
@@ -278,31 +355,29 @@ def run(home: str, repo: str, n_releases: int, release_type: str):
         print(f"No github releases found, skipping {repo}")
         return
 
-    while len(github_releases) > n_releases:
-        github_releases.popitem()
-    releases_to_keep = github_releases
-
-    n_releases = len(releases_to_keep)
+    releases_to_keep = list(sorted(github_releases, key=lambda x: x.created_at, reverse=True))
 
     local_versions = set(get_local_versions(home=home, repo=repo))
+    print(f"Got {len(local_versions)} local versions: {local_versions}")
+
     if not local_versions:
-        # no local versions, just download last `n_releases` releases from github
-        print(f"{n_releases} will be downloaded: {releases_to_keep.keys()}")
-        for i, new in enumerate(releases_to_keep.values()):
-            print(f"Processing release {i + 1}/{n_releases}. {repo}:{new['tag_name']}")
+        # no local versions, just download last `n_releases` releases from GitHub
+        print(f"{n_releases} will be downloaded: {[item.tag for item in releases_to_keep]}")
+        for i, new in enumerate(releases_to_keep):
+            print(f"Processing release {i + 1}/{n_releases}. {repo}:{new.tag}")
             download(release_info=new, home=home, repo=repo)
         print("Done")
         return
 
-    versions_to_download = releases_to_keep.keys() - local_versions
-    print(f"About to download: {versions_to_download}")
+    tags_to_keep: set[str] = set(r.tag for r in releases_to_keep)
+    print(f"Should remain: {tags_to_keep}")
 
-    versions_to_delete = local_versions - releases_to_keep.keys()
-    print(f"Will be deleted: {versions_to_delete}")
+    versions_to_delete: set[str] = local_versions - tags_to_keep
+    if versions_to_delete:
+        print(f"Will be deleted: {versions_to_delete}")
 
-    for i, version in enumerate(versions_to_download):
-        release = releases_to_keep[version]
-        print(f"Downloading release {i + 1}/{len(versions_to_download)}. {repo}:{release['tag_name']}")
+    for i, release in enumerate(releases_to_keep):
+        print(f"Processing release {repo}:{release.tag}")  # {i + 1}/{len(versions_to_download)}
         download(release_info=release, home=home, repo=repo)
 
     for version in versions_to_delete:
@@ -310,7 +385,7 @@ def run(home: str, repo: str, n_releases: int, release_type: str):
         release_path = os.path.join(home, repo, version)
         shutil.rmtree(release_path)
 
-    print("Done")
+    print(f"Done with {repo}")
 
 
 def main():
